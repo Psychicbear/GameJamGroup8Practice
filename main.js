7d73284b65aa17c55f3afa38f9e2081548618e5e
Britney Gwynne <britneygwynne@MacBook-Pro.local>
2021-10-30 17:26:32 +1000

    add car and load images

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 //this is the one you'll write your code in make sure to change the script tag in index.html to use main.js instead of test.js
 
-
 function preload(){
-
+    carImg = loadImage('CarGameImages/car.png');
+    finishImg = loadImage('CarGameImages/finish.png');
+    grassImg = loadImage('CarGameImages/grass.png');
+    roadImg = loadImage('CarGameImages/road.png');
 }
 
 function setup(){
+    createCar();
 
 }
 
 function draw(){
-
+    drawSprites();
 }
+
