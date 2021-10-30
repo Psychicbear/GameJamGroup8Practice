5b2293a20a919850da26a23300cfc6997d3f3107
Riley <rjscho@live.com.au>
2021-10-30 17:28:54 +1000

    Added trackLoader.js

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 //this is the one you'll write your code in make sure to change the script tag in index.html to use main.js instead of test.js
 
+const RES = {x: 1280, y: 720}
+let finishPos = 0
 
 function preload(){
-
+    trackFile = loadStrings('track.txt')
+    grassImg = loadImage('CarGameImages/grass.png')
+    roadImg = loadImage('CarGameImages/road.png')
+    finishImg = loadImage('CarGameImages/finish.png')
+    carImg = loadImage('CarGameImages/car.png')
 }
 
 function setup(){
-
+    createCanvas(RES.x,RES.y)
+    background(255,255,255)
+    grassGroup = new Group()
+    roadGroup = new Group()
+    grassImg.resize(RES.x/12, RES.y/6)
+    roadImg.resize(RES.x/12, RES.y/6)
+    finishImg.resize(RES.x/12, RES.y/6)
+    loadTrack()
+    console.log(finishPos)
 }
 
 function draw(){
-
+    drawSprites()
 }
---
+++ trackLoader.js
@@ -0,0 +1,39 @@
+//setup grassGroup = new Group()
+//setup roadGroup = new Group()
+//preload trackFile = loadstrings('track.txt')
+//run loadTrack() in setup
+function loadTrack(){
+    tilesizeX = RES.x/12;
+    tilesizeY = RES.y/6;
+    x = tilesizeX/2;
+    y = tilesizeY/2;
+    trackFile.forEach(line => {
+        currentLine = splitTokens(line)
+        currentLine.forEach(char => {
+            switch(char){
+                case '0':
+                    //Create grass sprite
+                    tile = createSprite(x,y,tilesizeX,tilesizeY);
+                    tile.addImage('grass', grassImg);
+                    tile.addToGroup(grassGroup);
+                    break;
+                case '1':
+                    //Create road sprite
+                    tile = createSprite(x,y,tilesizeX,tilesizeY);
+                    tile.addImage('road', roadImg);
+                    tile.addToGroup(roadGroup);
+                    break;
+                case '2':
+                    //Create finish sprite
+                    tile = createSprite(x,y,tilesizeX,tilesizeY);
+                    finishPos = {x: x, y: y}
+                    tile.addImage('road', finishImg);
+                    tile.addToGroup(roadGroup);
+                    break;                    
+            }
+            x += tilesizeX
+        })
+        x = tilesizeX/2
+        y += tilesizeY
+    })
+}
